b085ce93ebf2531f157b1277db189e268885ac75
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-04-03 02:41:27 -0400

    Made updates to java, html and css.

--- assets/app.js
+++ assets/app.js
@@ -1,93 +1,42 @@
 $(document).ready(function () {
+    //Creating Javascript file that is connected to HTMl for date.//
+var today = moment().format("Apr 3rd 21");  
+console.log(today)
+$(".o-clock").append(today);
 
-    // Static
+var the9am = $("9am");
+var the10am = $("10am");
+var the11am = $("11am");
+var the12am = $("12am");
+var the1am = $("1pm");
+var the2am = $("2pm");
+var the3am = $("3pm");
+var the4am = $("4pm");
+var the5am = $("5pm");
 
-    // The first basic method of Moment.js is the moment() method. Use this to get today's info!
-    console.log(moment());
+function Timescheduler() {
+    $("#today").moment().format("Apr 3rd 21");
 
-    console.log(moment().format());
+    $(".o-clock").each(function ()) {
+        var am = $(this).attr("am");
+        var planner = localStorage.getItem(am);
 
-    // Using moment format, there are different ways that we can display todays date!
-    $("#date").text(moment());
-    $("#date-formatted").text(moment().format());
-    $("#date-my").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+        if(planner !==null) {
+            $(this).children(".planner").val(planner);
+        }
+    });
 
-    $("#day-week").text(moment().format('do'));
-    $("#day-month").text(moment().format('Do'));
-    $("#day-year").text(moment().format('DDDo'))
+}
 
-    // Current Time
-    // Here we want to use setInterval to constantly update the time
-    let updateTime = function () {
-        let currentTime = moment().format('h:mm:ss')
-        $("#time").text(currentTime)
+
+var time = moment();
+
+function timeintervals() {
+    hour = time.numberofhours();
+    $(".o-clock").each(function () {
+        var currenthour = parseInt()
     }
+}
 
-    // Here we will get the number of hours in the year
-    let updateHour = function () {
-        let date = moment().dayOfYear()
-        let yearHours = date * 24
-        let todayHours = moment().hour();
-        let sumHours = yearHours + todayHours
-        $("#hours").text(sumHours)
-    }
 
-    // Here we will get the number of minutes in the week
-    let updateMinutes = function () {
-        let date = moment().weekday();
-        let weekMinutes = (date * 24 * 60) + (moment().minute());
-        $("#minutes").text(weekMinutes);
-    }
-
-    // Here we will get the number of seconds in the day
-    let updateSeconds = function () {
-        let todayHours = moment().hour();
-        let todaySeconds = todayHours * 60 * 60;
-        let thisSecond = moment().second();
-        let sumSeconds = thisSecond + todaySeconds;
-        $("#seconds").text(sumSeconds);
-    }
-
-    let countdown = function () {
-
-        // Friday
-        let friday = moment().day("fr")._d
-
-        // convert this into day of the year
-        let fridayDate = moment(friday).dayOfYear();
-
-        // This will get the milliseconds of Friday
-        let fridayMils = fridayDate * 24 * 60 * 60 * 1000
-        // Turn into seconds
-
-        // Today
-        // Turn into milliseconds
-
-        let date = moment().dayOfYear()
-        let yearMils = date * 24 * 60 * 60 * 1000
-        let todayMils = moment().hour() * 60 * 60 * 1000;
-        let thisMilsSecond = moment().second() * 1000;
-        let thisMils = moment().millisecond();
-
-        // This value should be increasing over time
-        let sumMils = yearMils + todayMils + thisMilsSecond + thisMils;
-
-        let ms = fridayMils - sumMils;
-        $("#countdown").text(ms)
-    }
-
-    // To initally set the times, we will call the functions
-    updateTime();
-    updateHour();
-    updateMinutes();
-    updateSeconds();
-    countdown();
-
-    // To continuously call the functions, we will use setInterval
-    setInterval(updateTime, 1000);
-    setInterval(updateHour, 1000);
-    setInterval(updateMinutes, 1000);
-    setInterval(updateSeconds, 1000);
-    setInterval(countdown, 1);
-
-})
+}
